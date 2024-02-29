3679323410207dea1b579e90b900e0b5ac5f99d6
Sujitkumar Verma <sujitkumar_verma@epam.com>
2024-02-29 23:23:20 +0530

    29th February 2024

--- Frontend/JavaScript/PsuedoCode/PsuedoCode.js
+++ Frontend/JavaScript/PsuedoCode/PsuedoCode.js
@@ -1,326 +1,323 @@
 //Question 1
-/* function fruit(){
-    console.log(name);
-    console.log(price);
-    var name = "Sujit";
-    let price = 20;
+function fruit() {
+  console.log(name);
+  console.log(price);
+  var name = "Sujit";
+  let price = 20;
 }
-fruit(); */
+fruit();
 
-/* 
-OUTPUT - 
-undefined
-ReferenceError Cannot access before decalartion
-It is giving ReferenceError because of Temporal Dead Zone
-*/
+// OUTPUT -
+// undefined
+// ReferenceError Cannot access before decalartion
+// It is giving ReferenceError because of Temporal Dead Zone
 
 //Question 2
-/* for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 3; i++) {
   setTimeout(() => console.log(i), 1);
-} */
+}
 
-/* 
-OUTPUT - 3 3 3
-Here the variable i is having global scope and setTimeout is executing at the
-end till that time the value of i would be 3. 
- */
+// OUTPUT - 3 3 3
+// The reason for this output is because of JavaScript's event loop and the concept of closures. When the
+// loop runs, it schedules three setTimeout tasks. These tasks will run asynchronously after at least 1
+// millisecond. By the time they run, the loop has already completed, and the value of i is 3 because the
+// loop terminates when i becomes equal to 3.
+
+// Since setTimeout is executed after the loop has finished, the callback function inside setTimeout retains
+// a reference to the variable i. When the callbacks execute, they use the current value of i, which is 3,
+// because it's the value that i had when the loop ended. This is an example of closure capturing the
+// variable i from its surrounding scope.
 
 //Question 3
-/* for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
   setTimeout(() => console.log(i), 1);
-} */
+}
 
-/* 
-OUTPUT - 0 1 2
-Here the variable i is having the local scope and setTimeout is having 
-different values of i for different Iteration of for loop.
-*/
+// OUTPUT - 0 1 2
+// This output differs from the previous example because let declares a block-scoped variable. In each
+// iteration of the loop, a new lexical environment is created, and i is bound to that environment. This
+// means that each iteration of the loop will have its own separate i variable.
+
+// When setTimeout executes, it creates a closure over the current value of i for each iteration of the
+// loop. Therefore, when the callback functions execute after the timeout, they use the correct value of
+// i that was captured during their respective iterations.
+
+// In summary, using let in the loop declaration prevents the common closure problem that occurs when using
+// var.
 
 //Question 4
-/* console.log(+true);
+console.log(+true);
 console.log(typeof +true);
-console.log(typeof typeof +true); */
+console.log(typeof typeof +true);
 
-/* 
-OUTPUT - 1 number string 
-The typeof of any datatype is string
-*/
+// OUTPUT - 1 number string
+// The typeof of any datatype is string
 
 //Question 5
-/* console.log(!"Sujit");
+console.log(!"Sujit");
 console.log(!!"Sujit");
-console.log(typeof("Sujit"));
- */
+console.log(typeof "Sujit");
 
-/* 
-OUTPUT - false true string 
-The boolean value of a non empty String is true
-*/
+// OUTPUT - false true string
+// The boolean value of a non empty String is true
 
 //Question 6
-/* let data  = "size";
+const data = "size";
 const bird = {
-    size:"small"
-}
+  size: "small",
+};
 
 console.log(bird[data]);
 console.log(bird["size"]);
 console.log(bird.size);
-console.log(bird.data); */
+console.log(bird.data);
 
-/* 
-OUTPUT - small small small undefined 
-*/
+// OUTPUT - small small small undefined
 
 //Question 7
-/* let c = {name:"Sujit Kumar Verma"};
+const c = { name: "Sujit Kumar Verma" };
 let d;
 d = c;
 c.name = "Allu Arjun";
-console.log(d); */
+console.log(d);
 
-/* 
-OUTPUT - { name: 'Allu Arjun' } 
-Here the We are the not copying the value of c into d but here we are assigning 
-the Reference of c into d.
-*/
+// OUTPUT - { name: 'Allu Arjun' }
+// Here the We are the not copying the value of c into d but here we are assigning
+// the Reference of c into d.
 
 //Question 8
-/* var x ;
+var x;
 var x = 10;
-console.log(x); */
+console.log(x);
 
-/* OUTPUT - 10 */
+// OUTPUT - 10
 
 //Question 9
-/* function fruit(){
-    console.log("Woof");
+function fruit() {
+  console.log("Woof");
 }
 
 fruit.name = "apple";
-fruit(); */
+fruit();
 
-/* OUTPUT - woof */
+// OUTPUT - woof
 
 //Question 10
-/* function getAge(...args){
-    console.log(args);
-    console.log(typeof args);
+function getAge(...args) {
+  console.log(args);
+  console.log(typeof args);
 }
-getAge(21); */
+getAge(21);
 
-/* 
-OUTPUT - [21] object 
-...args is a rest Operator which accepts infinite no of arguments as an Array.
-*/
+// OUTPUT - [21] object
+// ...args is a rest Operator which accepts infinite no of arguments as an Array.
 
 //Question 11
-/* const sum = eval('10*10+5');
-console.log(sum); */
+const sum = eval("10*10+5");
+console.log(sum);
 
-/* 
-OUTPUT - 105
-eval takes a String and performs the Operation of the Stringified values     
-*/
+// OUTPUT - 105
+// eval takes a String and performs the Operation of the Stringified values
 
 //Question 12
 
-/* const obj = {1:"a",2:"b",3:"c"};
+const obj = { 1: "a", 2: "b", 3: "c" };
 console.log(obj.hasOwnProperty("1"));
-console.log(obj.hasOwnProperty(1)); */
+console.log(obj.hasOwnProperty(1));
 
-/* 
-OUTPUT - true true 
-In hasOwnProperty we can specify the key as both String or as Numberic Value. 
-*/
+// OUTPUT - true true
+// In hasOwnProperty we can specify the key as both String or as Numberic Value.
 
 //Question 13
-/* const foo = () => console.log('First');
-const bar = () => setTimeout(()=>{console.log('Second')});
-const baz = () => console.log('Third');
+const foo = () => console.log("First");
+const bar = () =>
+  setTimeout(() => {
+    console.log("Second");
+  });
+const baz = () => console.log("Third");
 
 bar();
 foo();
-baz(); */
+baz();
 
-/* 
-OUTPUT - First Third Second
-No Matter whether we specify the timer in setTimeout or not it will get 
-executed at the End. 
-*/
+// OUTPUT - First Third Second
+// No Matter whether we specify the timer in setTimeout or not it will get executed at the End.
 
 //Question 14
-/* function sayHi() {
+function sayHi() {
   return (() => 100)();
 }
 console.log(typeof sayHi());
-*/
 
-/* 
-OUTPUT - 
-number  
-*/
+// OUTPUT -
+// number
 
 //Question 15
-/* const num = [1,2,3];
+const num = [1, 2, 3];
 num[9] = 100;
-console.log(num); */
+console.log(num);
 
-/* 
-OUTPUT - 
-[ 1, 2, 3, <6 empty items>, 100 ] 
-*/
+// OUTPUT -
+// [ 1, 2, 3, <6 empty items>, 100 ]
 
 //Question 16
-/* console.log(!!null);
+console.log(!!null);
 console.log(!!"");
 console.log(!!0);
 console.log(!!"Sujit");
 console.log(!!1);
 console.log(!!{});
-console.log(!!{name:"Sujit"}); */
+console.log(!!{ name: "Sujit" });
 
-/* 
-OUTPUT - 
-false
-false
-false
-true 
-true 
-true 
-true 
-*/
+// OUTPUT -
+// false
+// false
+// false
+// true
+// true
+// true
+// true
 
 //Question 17
-/* console.log(setInterval(()=>{console.log("Hi")},1000));
-console.log(setInterval(()=>{console.log("Hi")},1000));
-console.log(setInterval(()=>{console.log("Hi")},1000)); */
+console.log(
+  setInterval(() => {
+    console.log("Hi");
+  }, 1000)
+);
+console.log(
+  setInterval(() => {
+    console.log("Hi");
+  }, 1000)
+);
+console.log(
+  setInterval(() => {
+    console.log("Hi");
+  }, 1000)
+);
 
-
-/* 
-OUTPUT - 
-2 3 4 Hi Hi Hi ... 
-*/
+// OUTPUT -
+// 2 3 4 Hi Hi Hi ...
 
 //Question 18
-/* console.log([..."Sujit"]); */
+console.log([..."Sujit"]);
 
-/* 
-OUTPUT - 
-[ 'S', 'u', 'j', 'i', 't' ]
-Here we are using Spread Operator so It will spread the string into 
-indivisual character array.
- */
+// OUTPUT -
+// [ 'S', 'u', 'j', 'i', 't' ]
+// Here we are using Spread Operator so It will spread the string into indivisual character array.
 
 //Question 19
-/* console.log(3 + 4 + "5");
+console.log(3 + 4 + "5");
 console.log("5" + 3);
 console.log("5" - 3);
 console.log(3 - "5");
 console.log("5" * 4);
-console.log(5+4 * "5");
- */
+console.log(5 + 4 * "5");
 
-/* 
-OUTPUT - 
-75
-53
-2
--2
-20
-25 */
+// OUTPUT -
+// 75
+// 53
+// 2
+// -2
+// 20
+// 45
 
 //Question 20
-/* console.log([]===[]); */
+// console.log([2] === [2]);
 
-/* OUTPUT- false 
-Here both the arrays are having different memory location*/
+// OUTPUT- false
+// Here both the arrays are having different memory location
 
 //Question 21
-/* (() => {
+(() => {
   let x = (y = 10);
 })();
-console.log(typeof x); */
+console.log(typeof x);
 
-/* 
-OUTPUT - 
-undefined 
-Here the scope of x is local so that why we will get undefined for x in the global
-Scope.
-*/
+// OUTPUT -
+// undefined
+// Here the scope of x is local so that why we will get undefined for x in the global
+// Scope.
 
-
-//Question 22 
-/* (()=>{
-    let x = y = 21;
+//Question 22
+(() => {
+  let x = (y = 21);
 })();
 console.log(typeof y);
-console.log(y); */
+console.log(y);
 
-/* 
-OUTPUT - 
-number 21 
-Here y is not declared so by default it will get appended to the the global scope.
-*/
+// OUTPUT -
+// number 21
+// Here y is not declared so by default it will get appended to the the global scope.
 
-//Question 23 
+//Question 23
 console.log(!true - true);
-console.log(true+10);
+console.log(true + 10);
 
-/* OUTPUT - -1 11 */
+// OUTPUT - -1 11
 
-//Question 24 
-/* var x = 100;
+//Question 24
+var x = 100;
 (() => {
   var x = 20;
-})()
-console.log(x); */
+})();
+console.log(x);
 
-/* OUTPUT - 100
-Here the value of x will be 100 because there will be seprate memory for 
-both inner and outer var x.  */
-
+// OUTPUT - 100
+// Here the value of x will be 100 because there will be seprate memory for both inner and outer var x.
 
 //Question 25
-/* var x = 100;
+var x = 100;
 (() => {
   x = 20;
-})()
-console.log(x); */
+})();
+console.log(x);
 
-/* OUTPUT - 20 
-Here the value of x will be 20 because we are not redeclaring variable with 
-var keyword so the global declared var variable will be modified */
-
+// OUTPUT - 20
+// Here the value of x will be 20 because we are not redeclaring variable with var keyword so the global
+// declared var variable will be modified
 
 //Question 26
-/* const length = 4;
+const length = 4;
 const numbers = [];
-for (var i = 0; i < length; i++);{
-numbers.push(i + 1);
+for (var i = 0; i < length; i++);
+{
+  numbers.push(i + 1);
 }
-console.log(numbers); */
+console.log(numbers);
 
-/* OUTPUT - [5]
-Here the value of i after for loop completion will be 4
-*/
+// OUTPUT - [5]
+// Here the value of i after for loop completion will be 4
 
 //Question 27
 
 let i;
 for (i = 0; i < 3; i++) {
-const log = () => {
-console.log(i);
+  const log = () => {
+    console.log(i);
+  };
+  setTimeout(log, 100);
 }
-setTimeout(log, 100);
-}
 
-/* OUTPUT - 3 3 3 
-Here for each iteration of the for loop the setTimeout will be registered in the web API
-and will execute log function after for loop will get finished and i is having global 
-scope so the value of i after for iteration will be 3.
-*/
+// OUTPUT - 3 3 3
+// Here for each iteration of the for loop the setTimeout will be registered in the web API
+// and will execute log function after for loop will get finished and i is having global
+// scope so the value of i after for iteration will be 3.
 
+// when performing map, filter, reduce, forEach of this array-
+// [100, true, 'Hello', false, '', undefined, null, 0, {}, []], what is the o/p?
 
+arr = [100, true, "Hello", false, "", undefined, null, 0, {}, []];
+const result1 = arr.map((item) => item);
+console.log(result1);
+// OUTPUT - [100, true, 'Hello', false, '', undefined, null, 0, {}, []]
+const result2 = arr.filter((item) => item);
+console.log(result2);
+// OUTPUT - [ 100, true, 'Hello', {}, [] ]
+const result3 = arr.reduce((item) => item);
+console.log(result1);
 
-
+const result4 = arr.forEach((item) => {
+  return item;
+});
+console.log(result4);
+// OUTPUT - undefined
