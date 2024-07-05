f8f743a95a1c5dd273a957de48786e2e5db5c03d
Sujitkumar.Verma <Sujitkumar.Verma@cantire.com>
2024-07-05 11:45:06 +0530

    5th July 2024

---
+++ Backend/Java/Java/Lambda and Stream APIs/lambda-stream-apis/src/stream_operations/ternary_operations/Count.java
@@ -0,0 +1,18 @@
+package stream_operations.ternary_operations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Count {
+
+    public static void main(String[] args){
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        Long count = numbers.stream().count();
+        System.out.println(count);
+    }
+}
+
+//Note - Counts the number of elements in the stream
+
+//Note - Combines the elements of the stream into a single result.
